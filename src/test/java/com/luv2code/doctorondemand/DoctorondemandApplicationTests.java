2ae25941381ddf2c1e1855be94cfb0c917f2a039
lzugaj <luka.zugaj7@gmail.com>
2020-10-14 17:16:33 +0200

    Initial commit

---
+++ src/test/java/com/luv2code/doctorondemand/DoctorondemandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.doctorondemand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorondemandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

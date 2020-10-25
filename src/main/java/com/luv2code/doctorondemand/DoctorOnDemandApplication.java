1503cb4a966e69fa163b63a6746c6826fc521fe2
lzugaj <luka.zugaj7@gmail.com>
2020-10-25 17:14:55 +0100

    Refator logic for implementing multiple profiles

--- src/main/java/com/luv2code/doctorondemand/DoctorOnDemandApplication.java
+++ src/main/java/com/luv2code/doctorondemand/DoctorOnDemandApplication.java
@@ -1,31 +1,12 @@
 package com.luv2code.doctorondemand;
 
-import com.luv2code.doctorondemand.config.profile.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoctorOnDemandApplication {
 
-    private final Config config;
-
-    @Autowired
-    public DoctorOnDemandApplication(Config config) {
-        this.config = config;
-    }
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .sources(DoctorOnDemandApplication.class)
-                .profiles("dev")
-                .run(args);
-    }
-
-    @Bean
-    public CommandLineRunner execute() {
-        return args -> config.setup();
+        SpringApplication.run(DoctorOnDemandApplication.class, args);
     }
 }
--- src/main/java/com/luv2code/doctorondemand/config/profile/Config.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.doctorondemand.config.profile;
-
-public interface Config {
-
-    void setup();
-
-}
